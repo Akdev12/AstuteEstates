bbf7ae73b7a4aed90f5488641936499ecfd7e7aa
Akdev12 <aravindkumar.chakali@gmail.com>
2024-05-25 15:52:46 +0530

    small changes in user model

--- api/models/user.module.js
+++ api/models/user.module.js
@@ -1,23 +1,27 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type:string,
-        required:true,
-        unique:true,
-    }
-    email:{
-        type:string,
-        required:true,
-        unique:true,
-    }
-    password:{
-        type:string,
-        required:true,
-        //unique:true,
-    }
-}, {timestamps:true});
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: string,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: string,
+      required: true,
 
-const user=mongoose.model('User',userSchema);
+      //unique:true,
+    },
+  },
+  { timestamps: true }
+);
+
+const user = mongoose.model("User", userSchema);
 
 export default User;
